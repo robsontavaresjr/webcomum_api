554862b42b1c3f22107bbce20ad5e893b203be29
Utilizador <robsont@me.com>
2019-09-24 18:23:20 +0100

    Atualizado para o dia 20190923

--- app.py
+++ app.py
@@ -8,11 +8,37 @@ api = Flask(__name__)
 def get_companies():
   return json.dumps(companies)
 
-@api.route('/genericos', methods=['GET'])
+@api.route('/b3Ryb3BfaHRsYWVoX29wdXJnX3NvY2lyZW5lZw==', methods=['GET'])
 def get_genericos():
-    with open('inicio_genericos.json') as data_file:
-        data = json.dump(data_file)
-    return data
+    with open('inicio_genericos.json', 'r', encoding='utf-8') as data_file:
+        data = json.loads(data_file.read())
+    return str(data)
+
+@api.route('/ZXRpY29zX2dydXBvX2hlYWx0aF9wb3J0bw==', methods=['GET'])
+def get_eticos():
+    with open('inicio_eticos.json', 'r', encoding='utf-8') as data_file:
+        data = json.loads(data_file.read())
+    return str(data)
+
+@api.route('/YXMgZmFybWFjaWFzIHRhbyByaWNhcw==', methods=['GET'])
+def get_ganho():
+    with open('inicio_ganho.json', 'r', encoding='utf-8') as data_file:
+        data = json.loads(data_file.read())
+    return str(data)
+
+@api.route('/YXNldWd1dHJvcCBhaWxpbWFmIGxhbm9pY2lkYXJ0IGFkIGFpY2FtcmFm', methods=['GET'])
+def get_otc():
+    with open('inicio_farmacia_familia.json', 'r', encoding='utf-8') as data_file:
+        data = json.loads(data_file.read())
+    return str(data)\
+
+@api.route('/P29hbnVvLXNhZGlkbmV2X3NlZGFkaW51', methods=['GET'])
+def get_vendas():
+    with open('inicio_vendas.json', 'r', encoding='utf-8') as data_file:
+        data = json.loads(data_file.read())
+    return str(data)
+
+
 
 if __name__ == '__main__':
     api.run()
