9eb1aef6aad7146c4f1cc3e30aa8a294f4ace0ab
unknown <robsont@me.com>
2019-09-24 09:55:09 +0100

    primeiro commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+
+from flask_api import FlaskAPI
+import pandas as pd
+
+
+app = FlaskAPI(__name__)
+
+
+@app.route('/example/')
+def example():
+    return {'hello': 'world'}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
